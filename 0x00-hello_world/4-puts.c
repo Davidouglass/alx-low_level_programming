e6f0c2ced9b0dc23c076240964a9fb0975d0ffb9
DavidObasi <obasidavid676@gmail.com>
2022-09-08 09:35:57 -0700

    My first Commit on C programming files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\n"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
