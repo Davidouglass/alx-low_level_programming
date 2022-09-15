763ddb4b16e75b3de90732efd26cdcefeb8cdfbf
DavidObasi <obasidavid676@gmail.com>
2022-09-15 10:12:58 -0700

    My nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
-/*
- * * print_number - Prints a number
+/**
+ * print_number - Prints a number
  *
- * * @n: The number to print
- *
- * */
-
+ * @n: The number to print
+ */
 void print_number(int n)
-
 {
 	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-
 		num = -num;
 	}
 	if (num > 9)
