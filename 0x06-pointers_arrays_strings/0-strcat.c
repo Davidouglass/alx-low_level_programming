f3590500d40907c8cfba0d8f6a9d6aee80182069
DavidObasi <obasidavid676@gmail.com>
2022-09-22 03:39:12 -0700

    My pointers arrays springs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,43 +1,20 @@
 #include "holberton.h"
 
-#include <stdio.h>
-
-
-
 /**
- *
- *  * main - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
+ */
 
-int main(void)
-
+char *_strcat(char *dest, char *src)
 {
+	int index = 0;
+	int dest_len = 0;
 
-		char s1[98] = "Hello ";
-
-			char s2[] = "World!\n";
-
-				char *p;
-
-
-
-					printf("%s\n", s1);
-
-						printf("%s", s2);
-
-							p = _strcat(s1, s2);
-
-								printf("%s", s1);
-
-									printf("%s", s2);
-
-										printf("%s", p);
-
-											return (0);
-
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
