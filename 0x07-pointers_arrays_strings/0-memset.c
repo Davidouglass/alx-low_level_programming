cb954b24e5e56d935aee5cbbcf5a504578226e19
DavidObasi <obasidavid676@gmail.com>
2022-09-27 08:49:48 -0700

    My corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,14 @@
  * @n:unsigned int
  * Return:char
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-       	{
+	{
 		s[i] = b;
-       	}
+	}
 	return (s);
 }
