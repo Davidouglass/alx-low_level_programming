c59c0f3e1bd447a1f03bca00e6fe754bb1f04826
DavidObasi <obasidavid676@gmail.com>
2022-09-28 08:07:05 -0700

    My recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,10 +7,13 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
+
 int _pow_recursion(int x, int y)
 {
+
 if (y < 0)
 return (-1);
+
 if (y == )
 return (1);
 return (x * _pow_recursion(x, y - 1));
